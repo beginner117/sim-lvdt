016be331fda1d313325b6bf673239e6fe40ec133
Kumar Akhil Kukkadapu <kumarakhil.kukkadapu@uantwerpen.be>
2022-03-09 11:17:50 +0100

    update

--- Kumar/plots_smallIP.py
+++ Kumar/plots_smallIP.py
@@ -7,9 +7,9 @@ import warnings
 warnings.filterwarnings('ignore')
 
 # output_file data : [0]-position, [1]-upp_out_vol, [2]-low_out_vol, [3]-inn_vol, [4]-Norm_Out_Sig, [5]-fit_err, [6]-norm-fit
-output_files = [ "sip_def_sml.txt", "sip_inn wid_25_sml.txt", "sip_inn wid_26_sml.txt"]
+output_files = ["sip_dis_28_lar.txt", "sip_dis_29_lar.txt", "sip_dis_30_lar.txt", "sip_dis_31_lar.txt"]
 
-legends = ["inn wid = 24(def)", "inn wid = 25", "inn wid = 26"]
+legends = ["out dist = 28.5", "out dist = 29.5", "out dist = 30.5", "out dist = 31.5"]
 inputdata = []
 
 #for i in range(0,len(output_files)):
@@ -27,17 +27,17 @@ print(np.array(inputdata[1][1]).real.tolist())
 print(np.array(inputdata[1][0]).real.tolist())
 print(len(output_files))
 
-#'''
+
+
 #print(plt.style.available)
 plt.style.use(['science','grid','notebook'])
-
 for i in range(0,len(output_files)):
     plt.plot(np.array(inputdata[i][0]).real.tolist(), np.array(inputdata[i][1]).real.tolist(), 'o-', label=legends[i])
 plt.ylabel('Upper Out Coil Voltage [V] ')
 plt.xlabel('Inner Coil Position [mm]')
 plt.legend()
-#plt.savefig("upp_out.png")
-#shutil.move("upp_out.png", r"C:\Users\kumar\OneDrive\Desktop\pi\lvdt\small, large ip\small_IP\res\innwidth_2")
+plt.savefig("upp_out.png")
+shutil.move("upp_out.png", r"C:\Users\kumar\OneDrive\Desktop\pi\lvdt\small, large ip\small_IP\res\outdist_lar")
 plt.show()
 
 for i in range(0,len(output_files)):
@@ -45,8 +45,8 @@ for i in range(0,len(output_files)):
 plt.ylabel('Lower Out Coil Voltage [V] ')
 plt.xlabel('Inner Coil Position [mm]')
 plt.legend()
-#plt.savefig("low_out.png")
-#shutil.move("low_out.png", r"C:\Users\kumar\OneDrive\Desktop\pi\lvdt\small, large ip\small_IP\res\innwidth_2")
+plt.savefig("low_out.png")
+shutil.move("low_out.png", r"C:\Users\kumar\OneDrive\Desktop\pi\lvdt\small, large ip\small_IP\res\outdist_lar")
 plt.show()
 
 for i in range(0,len(output_files)):
@@ -54,8 +54,8 @@ for i in range(0,len(output_files)):
 plt.ylabel('Inner Out Coil Voltage [V]')
 plt.xlabel('Inner Coil Position [mm]')
 plt.legend()
-#plt.savefig("Inn_vol.png")
-#shutil.move("Inn_vol.png", r"C:\Users\kumar\OneDrive\Desktop\pi\lvdt\small, large ip\small_IP\res\innwidth_2")
+plt.savefig("Inn_vol.png")
+shutil.move("Inn_vol.png", r"C:\Users\kumar\OneDrive\Desktop\pi\lvdt\small, large ip\small_IP\res\outdist_lar")
 plt.show()
 
 for i in range(0,len(output_files)):
@@ -63,18 +63,18 @@ for i in range(0,len(output_files)):
 plt.ylabel('Normalised Out Coil signal [V/mmA]')
 plt.xlabel('Inner Coil Position [mm]')
 plt.legend()
-#plt.savefig("norm_sig.png")
-#shutil.move("norm_sig.png", r"C:\Users\kumar\OneDrive\Desktop\pi\lvdt\small, large ip\small_IP\res\innwidth_2")
+plt.savefig("norm_sig.png")
+shutil.move("norm_sig.png", r"C:\Users\kumar\OneDrive\Desktop\pi\lvdt\small, large ip\small_IP\res\outdist_lar")
 plt.show()
 
 for i in range(0,len(output_files)):
     plt.plot(np.array(inputdata[i][0]).real.tolist(), np.array(inputdata[i][6]).real.tolist(), 'o-', label=legends[i])
 plt.ylabel('Normalised Fit Error [%]')
 plt.xlabel('Inner Coil Position [mm]')
-plt.ylim(0,0.4)
+plt.ylim(0,4)
 plt.legend()
-#plt.savefig("normfiterr.png")
-#shutil.move("normfiterr.png", r"C:\Users\kumar\OneDrive\Desktop\pi\lvdt\small, large ip\small_IP\res\innwidth_2")
+plt.savefig("normfiterr.png")
+shutil.move("normfiterr.png", r"C:\Users\kumar\OneDrive\Desktop\pi\lvdt\small, large ip\small_IP\res\outdist_lar")
 plt.show()
 
 
@@ -83,16 +83,21 @@ for i in range(0,len(output_files)):
 plt.ylabel('Fit Error [V/mmA]')
 plt.xlabel('Inner Coil Position [mm]')
 plt.legend()
-#plt.savefig("fiterr.png")
-#shutil.move("fiterr.png", r"C:\Users\kumar\OneDrive\Desktop\pi\lvdt\small, large ip\small_IP\res\innwidth_2")
+plt.savefig("fiterr.png")
+shutil.move("fiterr.png", r"C:\Users\kumar\OneDrive\Desktop\pi\lvdt\small, large ip\small_IP\res\outdist_lar")
 plt.show()
-#'''
+
+
 '''
 x2 = [8.5/22, 8.5/20, 10.5/24, 8.5/18, 10.5/22, 12.5/24, 10.5/20, 13.5/24, 12.5/22, 10.5/18, 13.5/22, 12.5/20, 13.5/20, 13.5/18]
 y2 = [22.499, 21.8, 28.73, 19.4, 27.3, 33.441, 26.7, 35.8, 31.8, 23.8, 33.8, 29.9007, 31.8, 35.8]
-plt.plot(x2, y2, 'o-')
-plt.xlabel("out coil width/inn coil width (ratio)")
-plt.ylabel("Slope (sensitivity)")
+z2 = [r"$(\frac{8.5}{22})$", r"$(\frac{8.5}{20})$", r"$(\frac{10.5}{24})$", r"$(\frac{8.5}{18})$", r"$(\frac{10.5}{22})$",r"$(\frac{12.5}{24})$", r"$(\frac{10.5}{20})$", r"$(\frac{13.5}{24})$", r"$(\frac{12.5}{22})$", r"$(\frac{10.5}{18})$", r"$(\frac{13.5}{22})$", r"$(\frac{12.5}{20})$", r"($\frac{13.5}{20})$", r"$(\frac{13.5}{18})$"]
+fig, ax = plt.subplots()
+ax.plot(x2, y2, 'o-')
+for i, txt in enumerate(z2):
+    ax.annotate(txt, (x2[i], y2[i]))
+ax.set_xlabel(r"$\frac{Out  Coil  Width}{Inn  Coil  Width}$")
+ax.set_ylabel("Slope (sensitivity)")
 plt.savefig("ratio.png")
 
 plt.show()
