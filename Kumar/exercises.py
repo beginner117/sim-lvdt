ffcf2734e9df7e3a549273018135c64ac3bca52e
kumar akhil <kumarakhil.kukkadapu@uantwerpen.be>
2023-05-26 17:19:36 +0200

    changing the syntax

--- Kumar/exercises.py
+++ Kumar/exercises.py
@@ -2,22 +2,15 @@ import sys
 sys.path.append('./modules/')
 import femm_simulation
 
+#[[no.of steps, step size, initial offset]]
 
+input_para = {'sensor_type' : ['VC_fields'], 'SaveFile' : False,
+            'file_names' : ['tr1'],
+            'default_design' : ['no'],
+            'type_or_parameter_OR_angle' : [0],
+            'TotalSteps_StepSize_Offset_OR_offset' : [[0, 1, 0]]}
 
- #[[no.of steps, step size, initial offset]]
+sim_code = femm_simulation.Position_sensor(sensor_type = input_para['sensor_type'], save=input_para['SaveFile'], sim_range={'steps_size_offset':input_para['TotalSteps_StepSize_Offset_OR_offset']},
+                                data = {'filename(s)':input_para['file_names'], 'is default':input_para['default_design'], 'design or parameter':input_para['type_or_parameter_OR_angle']})
 
-saveall = []
-input = {'sensor_type' : ['VC_only'], 'SaveFile' : False,
-            'file_names' : [ "tr4"],
-            'default_design' : ['yes'],
-            'type_or_parameter' : ['H'],
-            'TotalSteps_StepSize_Offset' : [ [6, 1, -1]]}
-
-
-sim_code = femm_simulation.Position_sensor(sensor_type = input['sensor_type'], save=input['SaveFile'], sim_range={'steps_size_offset':input['TotalSteps_StepSize_Offset']},
-                                data = {'filename(s)':input['file_names'], 'is default':input['default_design'], 'design or parameter':input['type_or_parameter']})
 sim_code.execute()
-
-
-
-#b = [['A_def_extend', 'yes', 'A']]  #[[file name:str, default design check:str, design type:str/design parameter:float]]
